d6c8ba1ea3bda272365167a9f92d314063adce5b
dsmmyq <100673593+dsmmyq@users.noreply.github.com>
2022-08-25 17:43:37 +0800

    Update Google.py

--- action/Google.py
+++ action/Google.py
@@ -2,7 +2,7 @@ import requests
 
 rawGoogle = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Google/Google.yaml").text
 
-rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,yt.be","DOMAIN-KEYWORD,www.google.").replace("DOMAIN-SUFFIX,ytimg.com","DOMAIN,lens.l.google.com").replace("DOMAIN-SUFFIX,youtu.be","DOMAIN,www.googleapis.com").replace("DOMAIN-SUFFIX,ytimg.com","DOMAIN,lens.l.google.com").replace("DOMAIN-SUFFIX,googlevideo.com","DOMAIN-SUFFIX,docs.google.com").replace("DOMAIN-SUFFIX,ggpht.cn","DOMAIN-SUFFIX,drive.google.com").replace("DOMAIN-SUFFIX,ggpht.com","DOMAIN-SUFFIX,googledrive.com")
+rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,yt.be","DOMAIN-KEYWORD,www.google.").replace("DOMAIN-SUFFIX,ytimg.com","DOMAIN,lens.l.google.com").replace("DOMAIN-SUFFIX,youtu.be","DOMAIN,www.googleapis.com").replace("DOMAIN-SUFFIX,ytimg.com","DOMAIN,lens.l.google.com").replace("DOMAIN-SUFFIX,googlevideo.com","DOMAIN-SUFFIX,docs.google.com").replace("DOMAIN-SUFFIX,ggpht.cn","DOMAIN-SUFFIX,drive.google.com").replace("DOMAIN-SUFFIX,ggpht.com","DOMAIN-SUFFIX,googledrive.com").replace("IP-CIDR","").replace("PROCESS-NAME","").replace("DOMAIN-KEYWORD","")
 
 result = rawGoogle.split("\n")
 
