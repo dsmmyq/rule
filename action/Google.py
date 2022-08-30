8f04c91965a23a4ce0940f9b989dbea73ee23ce3
dsmmyq <100673593+dsmmyq@users.noreply.github.com>
2022-08-30 10:35:45 +0800

    Update Google.py

--- action/Google.py
+++ action/Google.py
@@ -2,14 +2,16 @@ import requests
 
 rawGoogle = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Google/Google.yaml").text
 
+rawYoutube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube.yaml").text
 rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,yt.be","DOMAIN-KEYWORD,www.google.")
 rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,ytimg.com","DOMAIN,lens.l.google.com")
 rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,youtu.be","DOMAIN,www.googleapis.com")
 rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,googlevideo.com","DOMAIN-SUFFIX,docs.google.com")
 rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,ggpht.cn","DOMAIN-SUFFIX,drive.google.com")
 rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,ggpht.com","DOMAIN-SUFFIX,googledrive.com")
-
-result = rawGoogle.split("\n")
+rawYoutube = rawYoutube.replace("IP-CIDR,172.110.32.0/21","").replace("- IP-CIDR,216.73.80.0/20","")
+rawYoutube = rawYoutube.replace("IP-CIDR,IP-CIDR6,2620:120:e000::/40","")
+result = rawGoogle.split("\n") + rawYoutube.split("\n")
 
 with open("./Google.yaml", "w") as f:
     f.write("\n".join(result))
