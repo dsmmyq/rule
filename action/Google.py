4e60b61f49cce8e48d7bec9cd40405c622f218e5
dsmmyq <100673593+dsmmyq@users.noreply.github.com>
2022-09-01 15:05:20 +0800

    Update Google.py

--- action/Google.py
+++ action/Google.py
@@ -2,10 +2,10 @@ import requests
 
 rawGoogle = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Google/Google.yaml").text
 rawGoogleDrive = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GoogleDrive/GoogleDrive.yaml").text
-rawGoogleVoice = requests.get("https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Clash/GoogleVoice/GoogleVoice.yaml").text
-rawGoogleSearch = requests.get("https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Clash/GoogleSearch/GoogleSearch.yaml").text
-rawGoogleEarth = requests.get("https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Clash/GoogleEarth/GoogleEarth.yaml").text
-rawGoogleFCM = requests.get("https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Clash/GoogleFCM/GoogleFCM.yaml").text
+rawGoogleVoice = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GoogleVoice/GoogleVoice.yaml").text
+rawGoogleSearch = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GoogleSearch/GoogleSearch.yaml").text
+rawGoogleEarth = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GoogleEarth/GoogleEarth.yaml").text
+rawGoogleFCM = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GoogleFCM/GoogleFCM.yaml").text
 rawYoutube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube.yaml").text
 rawGoogleDrive = rawGoogleDrive.replace("payload:","")
 rawGoogleVoice = rawGoogleVoice.replace("payload:","")
