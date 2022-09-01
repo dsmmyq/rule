9589f725e3e7802e7fafe1fffddeb31b4175407f
dsmmyq <100673593+dsmmyq@users.noreply.github.com>
2022-09-01 15:17:08 +0800

    Update Google.py

--- action/Google.py
+++ action/Google.py
@@ -9,7 +9,7 @@ rawGoogleFCM = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_
 rawYoutube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube.yaml").text
 
 result = ['payload:']
-for rawresult in [rawGoogleDrive, rawGoogleVoice, rawGoogleSearch, rawGoogleEarth, rawGoogleFCM, rawYoutube]:
+for rawresult in [rawGoogle, rawGoogleDrive, rawGoogleVoice, rawGoogleSearch, rawGoogleEarth, rawGoogleFCM, rawYoutube]:
     result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('payload:'))])
 result_text = '\n'.join(result)
 
