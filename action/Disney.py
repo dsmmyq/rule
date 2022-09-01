b754e6b0675ee6bca43ee674c8834996dd4d15b8
dsmmyq <100673593+dsmmyq@users.noreply.github.com>
2022-09-01 15:19:47 +0800

    Delete Disney.py

--- action/Disney.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawDisney = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Disney/Disney.yaml").text
-
-result = rawDisney.split("\n")
-
-with open("./Disney.yaml", "w") as f:
-    f.write("\n".join(result))
