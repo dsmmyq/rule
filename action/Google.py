a4c8a3e10e66fea6f7fb0f52d2d184ac427133e6
dsmmyq <100673593+dsmmyq@users.noreply.github.com>
2022-09-01 15:02:27 +0800

    Update Google.py

--- action/Google.py
+++ action/Google.py
@@ -1,18 +1,19 @@
 import requests
 
 rawGoogle = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Google/Google.yaml").text
-
+rawGoogleDrive = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GoogleDrive/GoogleDrive.yaml").text
+rawGoogleVoice = requests.get("https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Clash/GoogleVoice/GoogleVoice.yaml").text
+rawGoogleSearch = requests.get("https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Clash/GoogleSearch/GoogleSearch.yaml").text
+rawGoogleEarth = requests.get("https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Clash/GoogleEarth/GoogleEarth.yaml").text
+rawGoogleFCM = requests.get("https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Clash/GoogleFCM/GoogleFCM.yaml").text
 rawYoutube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube.yaml").text
-rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,yt.be","DOMAIN-KEYWORD,www.google.")
-rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,ytimg.com","DOMAIN,lens.l.google.com")
-rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,youtu.be","DOMAIN,www.googleapis.com")
-rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,googlevideo.com","DOMAIN-SUFFIX,docs.google.com")
-rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,ggpht.cn","DOMAIN-SUFFIX,drive.google.com")
-rawGoogle = rawGoogle.replace("DOMAIN-SUFFIX,ggpht.com","DOMAIN-SUFFIX,googledrive.com")
-rawYoutube = rawYoutube.replace("IP-CIDR,172.110.32.0/21","").replace("- IP-CIDR,216.73.80.0/20","")
-rawYoutube = rawYoutube.replace("IP-CIDR,IP-CIDR6,2620:120:e000::/40","")
+rawGoogleDrive = rawGoogleDrive.replace("payload:","")
+rawGoogleVoice = rawGoogleVoice.replace("payload:","")
+rawGoogleSearch = rawGoogleSearch.replace("payload:","")
+rawGoogleEarth = rawGoogleEarth.replace("payload:","")
+rawGoogleFCM = rawGoogleFCM.replace("payload:","")
 rawYoutube = rawYoutube.replace("payload:","")
-result = rawGoogle.split("\n") + rawYoutube.split("\n")
+result = rawGoogle.split("\n") + rawYoutube.split("\n") + rawGoogleDrive.split("\n") + rawGoogleVoice.split("\n") + rawGoogleFCM.split("\n") + rawGoogleSearch.split("\n") + rawGoogleEarth.split("\n")
 
 
 with open("./Google.yaml", "w") as f:
