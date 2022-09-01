c3b5535d4fa7b7884904eb47d1107e69c6118f04
dsmmyq <100673593+dsmmyq@users.noreply.github.com>
2022-09-01 15:36:49 +0800

    Update and rename main.py to Global.py

--- action/main.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawnetflix = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Netflix/Netflix.yaml").text
-
-result = rawnetflix.split("\n") 
-
-with open("./netflix.yaml", "w") as f:
-    f.write("\n".join(result))
