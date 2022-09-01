eacc3acccea1d3190b5b4a4ba1b5d0fffd764a87
dsmmyq <100673593+dsmmyq@users.noreply.github.com>
2022-09-01 15:18:18 +0800

    Update Netflix.py

--- action/Netflix.py
+++ action/Netflix.py
@@ -1,8 +1,12 @@
 import requests
 
 rawNetflix = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Netflix/Netflix.yaml").text
+rawDisney = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Disney/Disney.yaml").text
 
-result = rawNetflix.split("\n") 
+result = ['payload:']
+for rawresult in [rawNetflix, rawDisney]:
+    result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('payload:'))])
+result_text = '\n'.join(result)
 
 with open("./Netflix.yaml", "w") as f:
     f.write("\n".join(result))
